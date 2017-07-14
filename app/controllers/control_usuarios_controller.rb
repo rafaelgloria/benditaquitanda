62de2b674223a40049baacbc4090457d542258cf
Rafael Gloria <rafael.gloria@serpro.gov.br>
2017-07-14 16:04:03 -0300

    menu e pedido

--- app/controllers/control_usuarios_controller.rb
+++ app/controllers/control_usuarios_controller.rb
@@ -1,21 +1,9 @@
 class ControlUsuariosController < ApplicationController
   before_action :authenticate_usuario!
 
-<<<<<<< HEAD
-  #before_filter :set_current_user
-
-  #def set_current_user
-  #  Team.current_user = current_user
-  #end
-  
-  def index
-  	@usuarios = Usuario.all
-  #	authorize @usuarios
-=======
   def index
   	@usuarios = Usuario.all
   	authorize @usuarios
->>>>>>> 68b909a772c322cc04d8a6a94ba5e1c209a94d54
   end
 
 end
--- app/models/encomenda.rb
+++ app/models/encomenda.rb
@@ -1,3 +1,7 @@
 class Encomenda < ActiveRecord::Base
   belongs_to :endereco
+
+   def encomenda_com_endereco
+    "#{endereco.logradouro}, n° #{endereco.numero}"
+  end
 end
