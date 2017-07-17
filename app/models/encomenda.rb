53d2671e58b196ebcbdcf41e64d72e39ced8c23e
Rafael Gloria <rafael.gloria@serpro.gov.br>
2017-07-17 09:13:27 -0300

    diversos

--- app/models/encomenda.rb
+++ app/models/encomenda.rb
@@ -2,6 +2,6 @@ class Encomenda < ActiveRecord::Base
   belongs_to :endereco
 
    def encomenda_com_endereco
-    "#{endereco.logradouro}, n° #{endereco.numero}"
+    "#{endereco.logradouro}, n° #{endereco.numero} - #{dataentrega.strftime("%d/%m/%Y às %H:%M")}"
   end
 end
